a128f317b17748be0ffee13b6f0b6da08c34f407
JadenPo <54308076+JadenPo@users.noreply.github.com>
2021-05-09 15:43:57 +0800

    dd

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,60 +1,83 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import 'antd/dist/antd.css';
-import { Button, List, Input } from 'antd';
+import React from "react";
+import { connect } from "react-redux";
+import "antd/dist/antd.css";
+import { Button, List, Input } from "antd";
 
-const TodoList = (props) => { 
-    const { inputValue, list, changeInputValue, handleClick, handleDelete } = props;
+const TodoList = (props) => {
+    const {
+        inputValue,
+        list,
+        changeInputValue,
+        handleClick,
+        handleDelete,
+    } = props;
 
-    return ( 
-        <div style={{marginTop:'10px', marginLeft:'10px'}}>
+    return (
+        <div style={{ marginTop: "10px", marginLeft: "10px" }}>
             <div>
-                <Input placeholder="Basic usage" style={{width:"300px"}} value={inputValue} onChange={changeInputValue}/>
-                <Button style={{marginBottom:'5px'}} type="primary" onClick={handleClick}>Submit</Button>
+                <Input
+                    placeholder='Basic usage'
+                    style={{ width: "300px" }}
+                    value={inputValue}
+                    onChange={changeInputValue}
+                />
+                <Button
+                    style={{ marginBottom: "5px" }}
+                    type='primary'
+                    onClick={handleClick}
+                >
+                    Submit
+                </Button>
             </div>
             <List
-                style={{width:'375px'}}
+                style={{ width: "375px" }}
                 bordered
                 dataSource={list}
                 renderItem={(item, index) => (
-                    <List.Item onClick={() => {handleDelete(index)}} key={item}>
+                    <List.Item
+                        onClick={() => {
+                            handleDelete(index);
+                        }}
+                        key={item}
+                    >
                         {index}. {item}
                     </List.Item>
                 )}
-            />  
-        </div> );
-    }
+            />
+        </div>
+    );
+};
 
 const mapStateToProps = (state) => ({
-        inputValue: state.inputValue,
-        list: state.list
-})
+    inputValue: state.inputValue,
+    list: state.list,
+});
 
 const mapDispatchToProps = (dispatch) => {
     return {
         changeInputValue(e) {
             const action = {
-                type: 'change_input_value',
-                value: e.target.value
+                type: "change_input_value",
+                value: e.target.value,
             };
             dispatch(action);
         },
 
         handleClick() {
             const action = {
-                type: 'add_item'
+                type: "add_item",
             };
             dispatch(action);
         },
 
         handleDelete(index) {
             const action = {
-                type: 'delete_item',
-                index
+                type: "delete_item",
+                index,
             };
             dispatch(action);
-        }
-    }
-}
- 
+        },
+    };
+};
+
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,30 +1,30 @@
 const defaultState = {
-    inputValue: '',
-    list: []
+    inputValue: "",
+    list: [],
 };
 
 const reduer = (state = defaultState, action) => {
-    if(action.type === 'change_input_value') {
+    if (action.type === "change_input_value") {
         const newState = JSON.parse(JSON.stringify(state));
         newState.inputValue = action.value;
         return newState;
     }
 
-    if(action.type === 'add_item') {
-        if(!state.inputValue) return state;
+    if (action.type === "add_item") {
+        if (!state.inputValue) return state;
         const newState = JSON.parse(JSON.stringify(state));
         newState.list.push(newState.inputValue);
-        newState.inputValue = '';
+        newState.inputValue = "";
         return newState;
     }
 
-    if(action.type === 'delete_item') {
+    if (action.type === "delete_item") {
         const newState = JSON.parse(JSON.stringify(state));
         newState.list.splice(action.index, 1);
         return newState;
     }
 
     return state;
-} 
+};
 
 export default reduer;
